cd655211f13bf2d05c98ca65495ed7a546a814a4
Mateus Leandro <mateuslsvieira@hotmail.com>
2021-10-12 09:27:12 -0300

    category & specificcation modules

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,11 @@
 import express from "express";
 
+import { router } from "./routes";
+
 const app = express();
 
-app.listen(3333, () => console.log("Server is running!"));
+app.use(express.json());
+
+app.use(router);
+
+app.listen(3334, () => console.log("Server is running!"));
